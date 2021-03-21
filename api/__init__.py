eb202db365ddc916a9e0d7051e509cd86f2d3708
Guilherme Yoshida <64761267+guiyshd@users.noreply.github.com>
2021-03-20 23:46:55 -0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -10,8 +10,15 @@ def create_api(extra_config_settings={}):
 
     CORS(api)
 
-    @api.route('/ping', methods=['GET'])
-    def ping_pong():
-        return jsonify('pong!')
+    @api.route('/resize', methods=['GET', 'POST'])
+    def resize():
+        response_object = {'status': 'success'}
+        if request.method == 'POST':
+            post_data = request.get_json()
+
+            response_object['message'] = 'Book added!'
+        else:
+            response_object['books'] = BOOKS
+        return jsonify(response_object)
 
     return api
