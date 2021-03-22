6cf3d8be65ca064b36fcc497d0d89f686090485d
Guilherme Yoshida <64761267+guiyshd@users.noreply.github.com>
2021-03-22 09:31:02 -0300

    API Image View

--- api/__init__.py
+++ api/__init__.py
@@ -10,15 +10,7 @@ def create_api(extra_config_settings={}):
 
     CORS(api)
 
-    @api.route('/resize', methods=['GET', 'POST'])
-    def resize():
-        response_object = {'status': 'success'}
-        if request.method == 'POST':
-            post_data = request.get_json()
-
-            response_object['message'] = 'Book added!'
-        else:
-            response_object['books'] = BOOKS
-        return jsonify(response_object)
+    from api.images import bp
+    api.register_blueprint(api)
 
     return api
