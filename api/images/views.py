2b20160f8b4d350c5513fec1a72c73681bbaab59
Guilherme Yoshida <guiyshd@gmail.com>
2021-03-25 10:43:56 -0300

    Initial Client Side

--- api/images/views.py
+++ api/images/views.py
@@ -1,15 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 bp = Blueprint('image', __name__)
 
-@bp.route('/')
-def index():
-    # response_object = {'status': 'success'}
-    # if request.method == 'POST':
-    #     post_data = request.get_json()
-
-    #     response_object['message'] = 'Book added!'
-    # else:
-    #     response_object['books'] = BOOKS
-    return "Test"
+@bp.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello App!</h1>
+    <p>
+      <!-- use the router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+      <router-link to="/">Go to Home</router-link>
+      <router-link to="/ping">Go to Ping</router-link>
+    </p>
+
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
